8a2eaafb5f5f070ffe36e1543fee15c91e7d6ffd
derkaoui soumia <derkaoui.soumiaump.ac.ma>
2025-11-28 15:16:25 +0100

    Day 5 Completed

---
+++ Day_05/reverse_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void reverse_array(int arr[]) {
+    printf("Reversed array: ");
+    for (int i = 4; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    reverse_array(arr);
+    return 0;
+}
